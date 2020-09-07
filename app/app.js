f99518b7f388da589d395c9c180c577dfeddaa58
Deniz Mert Tecimer <deniz.tecimer@piworks.net>
2020-09-07 13:39:32 +0300

    fix(slider-element.module.js): imports moved to this file from app.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,5 @@
 import angular from 'angular';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
 import './slider/slider-element.module';
 
 angular.module('sliderDemo', ['sliderElement']);
+
--- app/slider/slider-element.module.js
+++ app/slider/slider-element.module.js
@@ -1,5 +1,11 @@
 import sliderElementComponent from './slider-element.component';
 import slideItemComponent from './slide-item/slide-item.component';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 const sliderModule = angular.module('sliderElement', [])
   .component('sliderElement', sliderElementComponent)
