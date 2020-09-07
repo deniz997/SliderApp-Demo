3be8653d5b5078f699eff049a05cd5d17bc218c4
Deniz Mert Tecimer <deniz.tecimer@piworks.net>
2020-09-07 10:22:34 +0300

    fix(slider-element.module.js): export statement added to the file

--- app/slider/slider-element.module.js
+++ app/slider/slider-element.module.js
@@ -1,6 +1,8 @@
 import sliderElementComponent from './slider-element.component';
 import slideItemComponent from './slide-item/slide-item.component';
 
-angular.module('sliderElement', [])
+const sliderModule = angular.module('sliderElement', [])
   .component('sliderElement', sliderElementComponent)
   .component('slideItem', slideItemComponent);
+
+export default sliderModule;
